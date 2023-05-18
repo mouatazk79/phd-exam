f44bcd0d635708622b3d88d0c8649efe0e24d523
klaa <mouatazk79@gmail.com>
2023-05-18 17:09:42 +0100

    fixing the cors issues

--- src/main/java/klaa/mouataz/edlli/SecurityApplication.java
+++ src/main/java/klaa/mouataz/edlli/SecurityApplication.java
@@ -1,8 +1,17 @@
 package klaa.mouataz.edlli;
 
-import klaa.mouataz.edlli.repos.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
+
 
 @SpringBootApplication
 public class SecurityApplication {
@@ -11,5 +20,19 @@ public class SecurityApplication {
 		SpringApplication.run(SecurityApplication.class, args);
 
 	}
+	@Bean
+	public FilterRegistrationBean simpleCorsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return bean;
+	}
+
 
 }
