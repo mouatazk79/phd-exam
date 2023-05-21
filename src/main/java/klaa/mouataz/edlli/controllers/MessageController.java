afebf9295a5e3adadc581c54ba0ce827bcdf23cf
klaa <mouatazk79@gmail.com>
2023-05-21 15:01:12 +0100

    fixing the message endpoint 4

--- src/main/java/klaa/mouataz/edlli/controllers/MessageController.java
+++ src/main/java/klaa/mouataz/edlli/controllers/MessageController.java
@@ -1,6 +1,7 @@
 package klaa.mouataz.edlli.controllers;
 
 import klaa.mouataz.edlli.model.Message;
+import klaa.mouataz.edlli.services.EnseignantService;
 import klaa.mouataz.edlli.services.MessageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.List;
 @RequestMapping("/api/v1/messages")
 public class MessageController {
     private final MessageService messageService;
+    private final EnseignantService enseignantService;
     @GetMapping
     public List<Message> getMessages(){
         return messageService.getAll();
@@ -20,8 +22,9 @@ public class MessageController {
     public Message getMessage(@PathVariable("id")Integer id){
         return messageService.getById(id);
     }
-    @PostMapping("/add")
-    public Message addMessage(@RequestBody Message message){
+    @PostMapping("/add/{id}")
+    public Message addMessage(@PathVariable("id")Integer id,@RequestBody Message message){
+        message.setEnseignant(enseignantService.getById(id));
         return messageService.save(message);
     }
     @DeleteMapping("/delete/{id}")
